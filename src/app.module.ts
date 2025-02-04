4b85bb98e7fc810f0b433c106e049262c57bd24a
Emanuel de Oliveira <oliveirabar.emanuel@gmail.com>
2025-02-04 20:59:08 -0300

    :heavy_minus_sign: build: remove TypeORM dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
 import { UserModule } from "./user/user.module";
 import { DatabaseModule } from "./database/database.module";
@@ -8,21 +7,7 @@ import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: process.env.DB_HOST || "localhost",
-      port: Number(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USER || "root",
-      password: process.env.DB_PASS || "senha",
-      database: process.env.DB_NAME || "letterboxd",
-      autoLoadEntities: true,
-      synchronize: false, // Deixa falso para criar as tabelas via SQL
-    }),
-    UserModule,
-    DatabaseModule,
-  ],
+  imports: [ConfigModule.forRoot(), UserModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
 })
